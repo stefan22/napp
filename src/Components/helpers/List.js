4d23a3055e3b0955d986241e818ca27833bf9c5f
stefan22 <stefan.emailacct@gmail.com>
2019-11-11 23:38:49 +0000

    gitusers list

---
+++ src/Components/helpers/List.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+
+class List extends Component {
+  render() {
+    const {items, item: Item} = this.props
+
+    return (
+      <>
+        {
+          items.map((itm,idx) =>
+            <Item
+              key={itm.id}
+              seckey={idx}
+              {...itm}
+
+            />
+
+          )
+        }
+      </>
+    )
+  }
+
+
+
+
+}
+
+
+export default List
