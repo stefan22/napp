0cc4b239a9b302b3701e75f9a06d87efeca61411
stefan22 <stefan.emailacct@gmail.com>
2019-11-25 23:04:26 +0000

    added dp path

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,13 +3,14 @@ import Dashboard from './Dashboard'
 import GitUser from './GitUser'
 import {Route, Switch} from 'react-router-dom'
 import '../scss/components/header.scss'
+import donPioPath from './helpers/donPioPath'
 
 
 const App = () => (
 
   <Switch>
 
-    <Route path={'/'}
+    <Route path={donPioPath()}
       component={Dashboard} exact
     />
     <Route path={'/git/:id'} exact
